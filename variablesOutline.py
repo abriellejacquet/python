025a9db4b7e23884defaa4779963072fb2821e76
abriellejacquet <93626816+abriellejacquet@users.noreply.github.com>
2021-11-19 10:01:36 -0600

    Update variablesOutline.py
    11/19/2021

--- variablesOutline.py
+++ variablesOutline.py
@@ -7,42 +7,50 @@ Fill in this outline as the instructor goes through the lesson.
 #Make an example variable of each of the types of variables (int, float, 
 #string, boolean and list):
 
-
-
+a = 5
+a = 3.45
+a = "Samantha"
+a = True
+a = [1,2,3,4]
 #2)
 #Make two variables one named 'a' and one named 'A' both set to  different data:
 #(Remember that capitalization matters, so these can be two different variables)
-
-
+varA = 1
+vara = 2
 
 #3)
 #Make a variable, and then go down a couple lines and change the data in that
 #same variable:
+var = 5
 
+var = "string"
 
 #4)
 #Make a variable and set it to a variable you have already created:
 
-
+var = varA
 
 #5)
 #Make a list with three items. Make a variable that is set to the second
 #item in that list:
 
-
+list = [1,2,3,4]
+var = (list[1])
 
 #6)
 #Make a String set to your name. Make a variable that is set to the first
 #character in that string:
 
-
+string = "Abrielle"
+var = "string[0]"
 
 #7)
 #Print out ONE of any of the variables you have created so far:
 
-
+print (var)
 
 #8)
-#Make a variable of any type, with one of the common SYNTAX errors
-
+#Make a variable of any type, with one of the common SYNTAX errorss
+#string = abrielle
+#no ""
 
