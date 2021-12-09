e109f9c5af9fb4899199d2a714f49c32ca68f7b6
abriellejacquet <93626816+abriellejacquet@users.noreply.github.com>
2021-12-08 20:21:17 -0600

    12/8/21

---
+++ survey.py
@@ -0,0 +1,21 @@
+'''
+Created on Dec 5, 2021
+
+@author: ITAUser
+'''
+#The objective is to make a program that can take survey information
+#from a user.
+
+name = input("What is your name?")
+
+
+color = input("What is your favorite color?")
+
+
+city = input("What city did you grow up in?")
+
+
+friend = input("What is your best friends name?")
+
+
+hair = input("What color hair do you have?")
