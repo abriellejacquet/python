e080e4c84a40b07172a8e12bbbb8ff4662640a4b
abriellejacquet <93626816+abriellejacquet@users.noreply.github.com>
2022-01-29 17:47:10 -0600

    1/29

---
+++ comparisonOutline.py
@@ -0,0 +1,46 @@
+'''
+This outline will help solidify concepts from the Comparison Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+from _socket import AF_APPLETALK
+from pip._vendor.tenacity import DoSleep
+
+#1) Make two string variables. Compare them using the == operator and store
+#that comparison in a new variable. Then print the variable.
+
+a= "apples" 
+b= "bananas" 
+x=(a==b)
+print(x)
+
+#2) Make two int variables. Compare them using the == operator and store
+#that comparison in a new variable. Then print the variable.
+
+e=(3==3)
+print(e)
+#3) Make two int variables. Compare them using the > operator and store
+#that comparison in a new variable. Then print the variable.
+c=(4>3)
+print(c)
+
+
+#4) Make two int variables. Compare them using the < operator and store
+#that comparison in a new variable. Then print the variable.
+d=(5<2)
+print(d)
+
+#5) Make two int variables. Compare them using the >= operator and store
+#that comparison in a new variable. Then print the variable.
+f=(6>=5)
+print(f)
+
+#6) Make two int variables. Compare them using the <= operator and store
+#that comparison in a new variable. Then print the variable.
+g=(9<=9)
+print(g)
+
+#7) Make a comparison with any operator, with one of the common SYNTAX errors.
+o="Dos" 
+p="dogs"
+j=(o==p)
+
