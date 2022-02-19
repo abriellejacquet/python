44ae71f06a043ffea2522e5df01c310500524fc5
abriellejacquet <93626816+abriellejacquet@users.noreply.github.com>
2022-02-19 15:23:25 -0600

    2/19

---
+++ logicalOutline.py
@@ -0,0 +1,43 @@
+'''
+This outline will help solidify concepts from the Logical Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+#EX) Make two boolean variables. Put them on either side of the and operator.
+#Store this expression in a variable named a. Print the variable.
+one = True
+two = False
+a = one and two 
+print(a)
+
+#1) Make two boolean variables. Put them on either side of the and operator.
+#Store this expression in a variable named a. Print the variable.
+leg = True
+arm = True
+a= leg and arm 
+print(a)
+
+
+
+#2) Make two boolean variables. Put them on either side of the or operator.
+#Store this expression in a variable named b. Print the variable.
+one = True 
+two = False 
+b = one or two 
+print(b)
+
+
+#3) Make one boolean variable. Put the variable after the not. Store this 
+#expression in a variable named c. Print the variable.
+c = not 3 == 3 
+print(c) 
+
+
+#4) Make a logical expression with one of the common SYNTAX errors.
+
+#messing up the comparison. I want the statement to be true.
+dog = 1
+cat = 2
+d= dog and cat 
+print(d) 
+
+# the and should be changed to an or for this statement to be true. 
